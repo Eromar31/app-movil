35b466b261e76c8f1153b94da8fd9f20da618a52
Eromar <erickpulachearevalo@gmail.com>
2026-06-23 19:24:14 -0500

    Actualizacion pantalla home cable tv

--- app/home.tsx
+++ app/home.tsx
@@ -10,32 +10,33 @@ export default function home() {
 
     },[]);
     return (
-    <view className="flex-1 bg-gray-100">
-        <View className="bg-blue-900 pt-10 pb-8 px-5 rounded-b-3xl">
-            <Text className="text-white text-3xl font-bold">
-                📡 Cable TV
+        <View className="flex-1 bg-gray-100">
+            <View className="bg-blue-900 pt-10 pb-8 px-5 rounded-b-3xl">
+                <Text className="text-white text-3xl font-bold">
+                    📡 Cable TV
+                </Text>
+
+                <Text className="text-blue-200 mt-1">
+                    Conectando los pueblos
+                </Text>
+            </View>
+
+            <View className="flex-1 justify-center items-center p-5">
+
+            <Text className="text-3xl font-bold">
+                Solicitudes
             </Text>
 
-            <Text className="text-blue-200 mt-1">
-                Conectando los pueblos
+            <Text className="mt-4 text-lg">
+                Bienvenido a TV conectando los pueblos
             </Text>
+
+            <CustomButton
+                titulo="Registrar Solicitud"
+                onPress={() => router.push("/registro")}
+            />
+            
+            </View>
         </View>
-
-        <View className="flex-1 justify-center items-center p-5">
-
-        <Text className="text-3xl font-bold">
-            Solicitudes
-        </Text>
-
-        <Text className="mt-4 text-lg">
-            Bienvenido a TV conectando los pueblos
-        </Text>
-
-        <CustomButton
-            titulo="Registrar Solicitud"
-            onPress={() => router.push("/registro")}
-        />
-        </View>
-    </view>
     );
 }
