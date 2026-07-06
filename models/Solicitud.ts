ae31a6cc61ab70dedce00b8bae678ca74aaa01a3
Eromar <erickpulachearevalo@gmail.com>
2026-07-05 20:36:39 -0500

    Actualización del proyecto de solicitudes

---
+++ models/Solicitud.ts
@@ -0,0 +1,21 @@
+export interface Solicitud {
+
+    id: number;
+
+    cliente: string;
+
+    telefono: string;
+
+    direccion: string;
+
+    tipoServicio: string;
+
+    prioridad: string;
+
+    descripcion: string;
+
+    estado: string;
+
+    fechaRegistro: string;
+
+}
